7180148b6df7998a462407ab0e46dadf1c0edf0b
Santiago Arraga <arraga@gmail.com>
2013-11-04 16:31:10 -0200

    reorg imports

--- butler/src/main/java/net/santablanca/butler/model/Bex.java
+++ butler/src/main/java/net/santablanca/butler/model/Bex.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
